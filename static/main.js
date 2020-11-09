e16045de388a271a8ea9962efde2d73ebb5fcabb
Mariana Marques <marianadacunhamarques@gmail.com>
2020-11-09 12:51:30 -0300

    change SCSS to CSS

--- static/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('#task').onkeyup = function() {
-    if (document.querySelector('#task').value === '') {
-        document.querySelector('#submit').disabled = true;
-    } else {
-        document.querySelector('#submit').disabled = false;
-    }
